f1dd8eae8ff42ff39850cbefe123a4c89cd2a034
yulekbutterfly <98512778+yulekbutterfly@users.noreply.github.com>
2022-04-26 12:10:19 +0300

    Добавьте файлы проекта.

---
+++ LangSchool/Data/ClientClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangSchool.Data
+{
+     public partial class Client
+    {
+        public DateTime? LastVisit
+        {
+            get
+            {
+                return ClientService.LastOrDefault()?.DateStart;
+            }
+      
+       }
+        public int CountVisit
+        {
+            get
+            {
+                return ClientService.Count();
+            }
+        }
+        public List<Tag> Tags
+        {
+            get
+            {
+                return Tag.ToList();
+            }
+        }
+
+    }  
+}
---
+++ LangSchool/Data/DataEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangSchool.Data
+{
+    class DataEntities
+    {
+        public static Entities context { get; } =new Entities();
+    }
+}
